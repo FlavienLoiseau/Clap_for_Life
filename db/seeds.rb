403735a6cf8cf049298edfefbf9e798332fbcd15
Martin-Louvard <martin.louvard@gmail.com>
2020-12-01 18:07:18 +0100

    Seed fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,6 +7,15 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+10.times do
+  user = User.create!(first_name: Faker::Name.first_name, email: Faker::Internet.email, admin: true, password: "foobar")
+  puts "User added"
+end
+
+10.times do
+  Activity.create(code: Faker::Code.npi, title: Faker::Lorem.words(number: 10), description: Faker::Lorem.words(number: 20))
+  puts "Activity added !"
+end
 
 10.times do
   organisation = Organisation.create(
@@ -30,22 +39,29 @@ require 'faker'
   puts "Mission created !"
 end
 
+5.times do
+  Tag.create(name: Faker::Lorem.words(number: 1), tagable_type: "Mission", tagable_id: rand(1..10) )
+  puts "Mission Tag created"
+end
+
+5.times do
+  Tag.create(name: Faker::Lorem.words(number: 1), tagable_type: "Organisation", tagable_id: rand(1..10) )
+  puts "Organisation Tag created"
+end
+
+
+
 10.times do
-  Tag.create(name: Faker::Lorem.words(number: 1))
-  puts "Tag created"
+  Address.create(number: rand(1..50), street: Faker::Address.street_name, city: Faker::Address.city, zipcode: Faker::Address.zip_code, country: "France", addressable_type: "Organisation", addressable_id: rand(1..10))
+  puts "Organisation Address added"
 end
 
 10.times do
-  user = User.create!(first_name: Faker::Name.first_name, email: Faker::Internet.email, admin: true, password: "foobar")
-  puts "User added"
+  Address.create(number: rand(1..50), street: Faker::Address.street_name, city: Faker::Address.city, zipcode: Faker::Address.zip_code, country: "France", addressable_type: "User", addressable_id: rand(1..10))
+  puts "User Address added"
 end
 
 10.times do
-  Activity.create(code: Faker::Code.npi, title: Faker::Lorem.words(number: 10), description: Faker::Lorem.words(number: 20))
-  puts "Activity added !"
-end
-
-10.times do
-  Address.create(number: rand(1..50), street: Faker::Address.street_name, city: Faker::Address.city, zipcode: Faker::Address.zip_code, country: "France")
-  puts "Address added"
+  Address.create(number: rand(1..50), street: Faker::Address.street_name, city: Faker::Address.city, zipcode: Faker::Address.zip_code, country: "France", addressable_type: "Mission", addressable_id: rand(1..10))
+  puts "Mission Address added"
 end
