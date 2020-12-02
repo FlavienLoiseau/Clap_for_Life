bb26395b63786cfd17a1c770afd8d8ed4acdf8b2
josephineremy <jremy2@audencia.com>
2020-12-02 20:18:40 +0100

    desinstall webpacker and dropdown ok

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//= require jquery
+//= require bootstrap.bundle
+//= require jquery.magnific-popup
+//= require smooth-scroll.polyfills
+//= require bootstrap-select
+//= require ofi
+//= require swiper
+//= require theme
