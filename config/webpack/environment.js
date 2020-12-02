3532046a3217f0d66aba02afefa61033cb3cf659
Martin-Louvard <martin.louvard@gmail.com>
2020-12-02 17:39:33 +0100

    fixed webpack bug

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,3 @@
-const webpack = require('webpack')
-environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
-)
+const { environment } = require('@rails/webpacker')
+
+module.exports = environment
