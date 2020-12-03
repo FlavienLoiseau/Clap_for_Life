0beb6b9d96e5ef54b82e7779e50a87e481c0c534
Martin-Louvard <martin.louvard@gmail.com>
2020-12-03 20:16:58 +0100

    Organisation index and search by tag

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -13,4 +13,17 @@ class Organisation < ApplicationRecord
   has_one_attached :cover
   has_many_attached :images
 
+  def self.search(search)
+    if search
+      tag = Tag.find_by(name: search)
+      if tag
+        Organisation.all.select{|o| o if o.tags.any? {|t| t.name == tag.name}}
+      else
+        Organisation.all
+      end
+    else
+      Organisation.all
+    end
+  end
+
 end
