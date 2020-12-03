4f26575f68093c8b41f8f9127547449092b23a3b
Martin-Louvard <martin.louvard@gmail.com>
2020-12-03 18:22:25 +0100

    Mission search by tag

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :users, only: [:show, :edit, :update]
-
+  resources :missions, only: [:index]
   resources :organisations, only: [:index, :show] do
     resources :missions, only: [:show]
   end
