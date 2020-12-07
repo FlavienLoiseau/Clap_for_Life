581230f34d44d76e4897c7608152be1756472a31
FlavienLoiseau <flavien.loiseau@edu.escp.eu>
2020-12-07 18:51:28 +0100

    all mailers runing except contact_page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,6 +10,7 @@ class HomeController < ApplicationController
   end
 
   def contact
+    UserMailer.contact_form(first_name: params[:first_name], last_name: params[:last_name], email: params[:email], email_content: params[:email_content])
   end
 
   def vision
