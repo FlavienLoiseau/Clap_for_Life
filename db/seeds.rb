90b4ba21a680badfccd7d24016d702c0a414fda5
FlavienLoiseau <flavien.loiseau@edu.escp.eu>
2020-12-10 09:25:53 +0100

    seed cleaning

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 Faker::Config.locale = :fr
 
@@ -18,7 +11,7 @@ User.destroy_all
 
 i=1
 15.times do
-  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, admin: true, password: "foobar", username: "user"+i.to_s)
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, admin: true, password: "foobar")
   puts "User added"
   i+=1
 end
